2e2d10e980dbfa7567abca0f448292b44377dc7c
Andres_Izurieta <andres.izurieta@hotmail.cl>
2024-01-28 15:07:51 -0300

    Update app.js
    Mismo que anterior update

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function generarNumeroSecreto() {
     let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
 
     //console.log(numeroGenerado);
-    console.log(listaNumerosSorteados);
+    //console.log(listaNumerosSorteados);
     //Si ya sorteamos todos los números
     if (listaNumerosSorteados.length == numeroMaximo) {
         asignarTextoElemento('p','Ya se sortearon todos los números posibles');
@@ -58,7 +58,7 @@ function condicionesIniciales() {
     asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
-    console.log(numeroSecreto);
+    //console.log(numeroSecreto);
 }
 
 function reiniciarJuego() {
